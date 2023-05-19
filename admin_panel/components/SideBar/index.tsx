b4f274609d4525cf072d00c3cf2f68b726f81366
Sunday Mgbogu <digitalsimboja@gmail.com>
2023-05-19 11:03:49 +0100

    implements collapse feature on the sidebar

--- admin_panel/components/SideBar/index.tsx
+++ admin_panel/components/SideBar/index.tsx
@@ -1,17 +1,27 @@
-// Sidebar component
 import Link from "next/link";
 import { FaHome, FaUserCog } from "react-icons/fa";
 import { RiBarChart2Fill } from "react-icons/ri";
 import { HiOutlineUserGroup } from "react-icons/hi";
+import { useState } from "react";
 
 interface SidebarProps {
   children: React.ReactNode;
 }
 
 export default function Sidebar(props: SidebarProps) {
+  const [collapsed, setCollapsed] = useState(false);
+
+  const toggleCollapse = () => {
+    setCollapsed(!collapsed);
+  };
+
   return (
     <div className="flex">
-      <div className="fixed w-32 sm:w-40 h-screen p-4 flex flex-col items-center justify-between bg-custom-yellow overflow-y-auto">
+      <div
+        className={`fixed w-32 sm:w-40 h-screen p-4 flex flex-col items-center justify-between bg-custom-yellow overflow-y-auto ${
+          collapsed ? "hidden" : ""
+        }`}
+      >
         <div className="flex flex-col items-center">
           <div className="mb-8">
             <Link href="/">
@@ -48,11 +58,38 @@ export default function Sidebar(props: SidebarProps) {
           </div>
         </div>
       </div>
-      <main className="ml-24 sm:ml-32 w-full z-10 bg-custom-gray h-screen overflow-hidden rounded-l-[40px] p-8">
+      <main
+        className={`ml-24 sm:ml-32 w-full z-10 bg-custom-gray h-screen overflow-hidden rounded-l-[40px] p-8 ${
+          collapsed ? "ml-4 sm:ml-8" : ""
+        }`}
+      >
         <div className="bg-custom-gray overflow-y-auto h-full">
           {props.children}
         </div>
       </main>
+      <button
+        className={`fixed top-4 right-4 sm:hidden bg-custom-yellow hover:bg-gray-200 cursor-pointer p-3 rounded-full z-20 ${
+          collapsed ? "rotate-180" : ""
+        }`}
+        onClick={toggleCollapse}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6 text-white"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d={collapsed ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16M4 18h16"}
+          />
+        </svg>
+        </button>
     </div>
   );
 }
+
+     
